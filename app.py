bff6e2a98ff27d66d45f46516002fe61a2097048
cheng.lu <cheng.lu@united-imaging.com>
2019-01-04 13:32:35 +0800

    04 static file update

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 
 name = "Lucheng"
 
 movies = [
         {'title': 'My neighbor totoro', 'year': '1988'},
-        {'title': 'The pork of music', 'year': '2012'}
+        {'title': 'The pork of music', 'year': '2012'},
+        {'title': 'WALL-E', 'year':'2008'}
 ]
-
 
 
 app = Flask(__name__)
 
-@app.route("/home")
 @app.route("/")
-@app.route("/index")
-def hello():
+def index():
     return render_template('index.html', name=name, movies=movies)
-
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
-
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='greyli'))
-
-
-    return 'Test page'
 
