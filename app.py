518690efb9db3a86535aafadc2989fec7bef1e40
cheng.lu <cheng.lu@united-imaging.com>
2019-04-02 17:55:16 +0800

    update last

--- app.py
+++ app.py
@@ -69,3 +69,8 @@ def index():
     movies = Movie.query.all()
     return render_template('index.html', user=user, movies=movies)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html', user=user), 404
